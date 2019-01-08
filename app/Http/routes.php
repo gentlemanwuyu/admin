33ab286339ede7b734e8cc96898525786cd68f8a
wuyu <yu.wu@patpat.com>
2019-01-08 17:25:24 +0800

    整合admin-lte页面代码

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/default', function () {
+    return view('layouts/default');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,8 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    //拷贝AdminLTE相关的文件和插件
+    mix.copy('node_modules/admin-lte/bower_components','public/assets/adminlte/bower_components');
+    mix.copy('node_modules/admin-lte/plugins','public/assets/adminlte/plugins');
+    mix.copy('node_modules/admin-lte/dist','public/assets/adminlte/dist');
 });
