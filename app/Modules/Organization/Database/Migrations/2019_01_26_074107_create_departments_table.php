a17ed2facd6d2a0eb9479f66a615228101364e7f
wuyu <yu.wu@patpat.com>
2019-01-30 19:38:19 +0800

    department表中的name字段改为唯一索引

--- app/Modules/Organization/Database/Migrations/2019_01_26_074107_create_departments_table.php
+++ app/Modules/Organization/Database/Migrations/2019_01_26_074107_create_departments_table.php
@@ -14,7 +14,7 @@ class CreateDepartmentsTable extends Migration
 	{
 		Schema::create('departments', function (Blueprint $table) {
 			$table->increments('id');
-			$table->string('name')->default('')->comment('部门名称');
+			$table->string('name')->default('')->comment('部门名称')->unique();
 			$table->smallInteger('parent_id')->default(0)->comment('上级ID');
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('创建时间');
 			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('最后更新时间');
