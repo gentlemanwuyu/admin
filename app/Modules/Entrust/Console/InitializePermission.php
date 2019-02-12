cb0146962325c4f8c24d7175d5277c8ac2816b98
wuyu <yu.wu@patpat.com>
2019-02-12 15:29:06 +0800

    权限初始化数据

--- app/Modules/Entrust/Console/InitializePermission.php
+++ app/Modules/Entrust/Console/InitializePermission.php
@@ -44,22 +44,94 @@ class InitializePermission extends Command
      */
     public function handle()
     {
-        // 添加用户权限
+        /**
+         * Menu权限
+         */
+        // 用户列表
+        Permission::create([
+            'name' => 'user_list',
+            'display_name' => 'User List',
+            'type_id' => 1,
+        ]);
+        // 角色列表
+        Permission::create([
+            'name' => 'role_list',
+            'display_name' => 'Role List',
+            'type_id' => 1,
+        ]);
+        // 权限列表
+        Permission::create([
+            'name' => 'permission_list',
+            'display_name' => 'Permission List',
+            'type_id' => 1,
+        ]);
+        // 部门结构图
+        Permission::create([
+            'name' => 'department_chart',
+            'display_name' => 'Department chart',
+            'type_id' => 1,
+        ]);
+
+        // 添加用户
         Permission::create([
             'name' => 'add_user',
             'display_name' => 'Add user',
             'type_id' => 2,
         ]);
-        // 编辑用户权限
+        // 编辑用户
         Permission::create([
             'name' => 'edit_user',
             'display_name' => 'Edit user',
             'type_id' => 2,
         ]);
-        // 删除用户权限
+        // 删除用户
         Permission::create([
             'name' => 'delete_user',
             'display_name' => 'Delete user',
+            'type_id' => 2,
+        ]);
+
+        // 添加权限
+        Permission::create([
+            'name' => 'add_permission',
+            'display_name' => 'Add permission',
+            'type_id' => 2,
+        ]);
+        // 修改权限
+        Permission::create([
+            'name' => 'edit_permission',
+            'display_name' => 'Edit permission',
+            'type_id' => 2,
+        ]);
+        // 删除权限
+        Permission::create([
+            'name' => 'delete_permission',
+            'display_name' => 'Delete permission',
+            'type_id' => 2,
+        ]);
+
+        // 添加部门
+        Permission::create([
+            'name' => 'add_department',
+            'display_name' => 'Add department',
+            'type_id' => 2,
+        ]);
+        // 修改部门
+        Permission::create([
+            'name' => 'edit_department',
+            'display_name' => 'Edit department',
+            'type_id' => 2,
+        ]);
+        // 删除部门
+        Permission::create([
+            'name' => 'delete_department',
+            'display_name' => 'Delete department',
+            'type_id' => 2,
+        ]);
+        // 拖动部门
+        Permission::create([
+            'name' => 'drag_department',
+            'display_name' => 'Drag department',
             'type_id' => 2,
         ]);
     }
