1edd3aa06d0848ad7bfe6929ff8105602185544b
wuyu <yu.wu@patpat.com>
2019-02-12 15:11:07 +0800

    配置entrust文件

---
+++ app/Modules/Entrust/Models/Role.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WuYu
+ * Date: 2019/2/12
+ * Time: 15:07
+ */
+
+namespace App\Modules\Entrust\Models;
+
+use Zizaco\Entrust\EntrustRole;
+
+class Role extends EntrustRole
+{
+    protected $guarded = [];
+}
