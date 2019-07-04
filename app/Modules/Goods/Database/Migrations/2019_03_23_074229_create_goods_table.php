86cb30c01cadf52394ea3e03ea29e636b2cb5cc3
wuyu <yu.wu@patpat.com>
2019-07-04 15:02:33 +0800

    商品表加上图片链接字段

--- app/Modules/Goods/Database/Migrations/2019_03_23_074229_create_goods_table.php
+++ app/Modules/Goods/Database/Migrations/2019_03_23_074229_create_goods_table.php
@@ -17,6 +17,7 @@ class CreateGoodsTable extends Migration
 			$table->string('code')->default('')->comment('商品编号');
 			$table->string('name')->default('')->comment('商品标题');
 			$table->text('description')->default('')->comment('商品描述');
+			$table->text('image_link')->default('')->comment('图片链接');
 			$table->enum('type',[0, 1, 2])->default(0)->comment('商品类型，1为singgle，2为combo');
 			$table->integer('category_id')->default(0)->comment('分类ID');
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('创建时间');
