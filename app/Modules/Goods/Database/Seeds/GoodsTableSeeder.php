8a8f5b036905a545447a497fdb4c66f48a0e1668
wuyu <492444775@qq.com>
2019-07-06 18:55:37 +0800

    商品数据填充1

--- app/Modules/Goods/Database/Seeds/GoodsTableSeeder.php
+++ app/Modules/Goods/Database/Seeds/GoodsTableSeeder.php
@@ -9,15 +9,18 @@
 namespace App\Modules\Goods\Database\Seeds;
 
 use Illuminate\Database\Seeder;
-use App\Modules\Goods\Models\Goods;
-use App\Modules\Goods\Models\SingleSku;
 use App\Modules\Product\Models\Product;
+use App\Modules\Goods\Models\Goods;
+use App\Modules\Goods\Models\GoodsSku;
+use App\Modules\Goods\Models\SingleProduct;
+use App\Modules\Goods\Models\SingleSkuProductSku;
 
 class GoodsTableSeeder extends Seeder
 {
     public function run()
     {
-//        $this->singleSeeder();
+        $this->singleSeeder();
+        $this->comboSeeder();
     }
 
     /**
@@ -25,26 +28,45 @@ class GoodsTableSeeder extends Seeder
      */
     public function singleSeeder()
     {
-        $products = Product::all();
+        $products = Product::whereIn('id', [1, 3, 5, 6, 7, 8])->get();
+        $category_relations = [
+            1 => 2,
+            3 => 3,
+            5 => 3,
+            6 => 6,
+            7 => 14,
+            8 => 16,
+        ];
         foreach ($products as $product) {
             $goods = Goods::create([
                 'code' => $product->code,
                 'name' => $product->name,
                 'type' => Goods::SINGLE,
-                'category_id' => $product->category_id,
+                'category_id' => $category_relations[$product->id],
+            ]);
+
+            SingleProduct::create([
+                'goods_id' => $goods->id,
                 'product_id' => $product->id,
             ]);
 
             foreach ($product->skus as $product_sku) {
-                SingleSku::create([
+                $goods_sku = GoodsSku::create([
                     'goods_id' => $goods->id,
-                    'product_sku_id' => $product_sku->id,
                     'code' => $product_sku->code,
                     'lowest_price' => $product_sku->cost_price * 2,
                     'msrp' => $product_sku->cost_price * 4,
-                    'enabled' => mt_rand(0, 1),
+                ]);
+                SingleSkuProductSku::create([
+                    'goods_sku_id' => $goods_sku->id,
+                    'product_sku_id' => $product_sku->id,
                 ]);
             }
         }
+    }
+
+    public function comboSeeder()
+    {
+
     }
 }
