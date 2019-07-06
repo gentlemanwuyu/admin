4829d9a8f06ad08a34d3c084b8a2e230a3a13309
wuyu <492444775@qq.com>
2019-07-06 22:36:20 +0800

    商品数据填充2

--- app/Modules/Goods/Database/Seeds/GoodsTableSeeder.php
+++ app/Modules/Goods/Database/Seeds/GoodsTableSeeder.php
@@ -8,6 +8,8 @@
 
 namespace App\Modules\Goods\Database\Seeds;
 
+use App\Modules\Goods\Models\ComboProduct;
+use App\Modules\Goods\Models\ComboSkuProductSku;
 use Illuminate\Database\Seeder;
 use App\Modules\Product\Models\Product;
 use App\Modules\Goods\Models\Goods;
@@ -67,6 +69,48 @@ class GoodsTableSeeder extends Seeder
 
     public function comboSeeder()
     {
+        // 滚轮片144
+        $goods = Goods::create(['code' => 'xhsglpbj144', 'name' => '包胶滚轮片144', 'type' => Goods::COMBO, 'category_id' => 15,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 11, 'quantity' => 1,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 12, 'quantity' => 1,]);
+        $goods_sku1 = GoodsSku::create(['goods_id' => $goods->id, 'code' => 'xhsglpbj144a', 'lowest_price' => 0.6, 'msrp' => 1.2,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 11, 'product_sku_id' => 29,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 12, 'product_sku_id' => 32,]);
+        $goods_sku2 = GoodsSku::create(['goods_id' => $goods->id, 'code' => 'xhsglpbj144b', 'lowest_price' => 0.6, 'msrp' => 1.2,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 11, 'product_sku_id' => 30,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 12, 'product_sku_id' => 32,]);
+        $goods_sku3 = GoodsSku::create(['goods_id' => $goods->id, 'code' => 'xhsglpbj144c', 'lowest_price' => 0.6, 'msrp' => 1.2,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 11, 'product_sku_id' => 31,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 12, 'product_sku_id' => 33,]);
 
+        // 流量计011
+        $goods = Goods::create(['code' => 'xhsllj011', 'name' => 'kingspring流量计011', 'type' => Goods::COMBO, 'category_id' => 45,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 17, 'quantity' => 1,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 18, 'quantity' => 1,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 19, 'quantity' => 1,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 20, 'quantity' => 2,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 21, 'quantity' => 2,]);
+        ComboProduct::create(['goods_id' => $goods->id, 'product_id' => 22, 'quantity' => 2,]);
+        $goods_sku1 = GoodsSku::create(['goods_id' => $goods->id, 'code' => 'xhsgllj01110-100lpm', 'lowest_price' => 120, 'msrp' => 240,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 17, 'product_sku_id' => 44,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 18, 'product_sku_id' => 47,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 19, 'product_sku_id' => 49,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 20, 'product_sku_id' => 51,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 21, 'product_sku_id' => 52,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku1->id, 'product_id' => 22, 'product_sku_id' => 54,]);
+        $goods_sku2 = GoodsSku::create(['goods_id' => $goods->id, 'code' => 'xhsgllj01115-150lpm', 'lowest_price' => 130, 'msrp' => 240,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 17, 'product_sku_id' => 45,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 18, 'product_sku_id' => 47,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 19, 'product_sku_id' => 49,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 20, 'product_sku_id' => 51,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 21, 'product_sku_id' => 52,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku2->id, 'product_id' => 22, 'product_sku_id' => 54,]);
+        $goods_sku3 = GoodsSku::create(['goods_id' => $goods->id, 'code' => 'xhsgllj01120-200lpm', 'lowest_price' => 140, 'msrp' => 260,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 17, 'product_sku_id' => 46,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 18, 'product_sku_id' => 48,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 19, 'product_sku_id' => 50,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 20, 'product_sku_id' => 51,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 21, 'product_sku_id' => 52,]);
+        ComboSkuProductSku::create(['goods_sku_id' => $goods_sku3->id, 'product_id' => 22, 'product_sku_id' => 54,]);
     }
 }
