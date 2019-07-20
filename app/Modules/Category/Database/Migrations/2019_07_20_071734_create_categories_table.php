924113e8049598f1666d46ec852ece7d7199441f
wuyu <492444775@qq.com>
2019-07-20 15:44:24 +0800

    fix

--- app/Modules/Category/Database/Migrations/2019_07_20_071734_create_categories_table.php
+++ app/Modules/Category/Database/Migrations/2019_07_20_071734_create_categories_table.php
@@ -21,6 +21,7 @@ class CreateCategoriesTable extends Migration
 			$table->tinyInteger('type')->default(0)->comment('类型，1为product，2为goods');
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('创建时间');
 			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('最后更新时间');
+			$table->timestamp('deleted_at')->nullable()->comment('删除时间');
 			$table->index('name');
 			$table->index('parent_id');
 		});
