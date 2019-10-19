7006d8627a281dc5f70e8177f9127193efbac341
WuYu <yu.wu@patpat.com>
2019-10-19 15:09:24 +0800

    添加collections

---
+++ app/libs/collections.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patpat
+ * Date: 2019/10/19
+ * Time: 14:59
+ */
+
+class Payment
+{
+    static $methods = [
+        1 => 'cash',
+        2 => 'after_deliver',
+        3 => 'monthly',
+    ];
+}
